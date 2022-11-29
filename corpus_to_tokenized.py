c30e5e83564ea366dc5139c308a3a9e43ea20f65
tolgarecep <tolgarecep@protonmail.com>
2022-11-29 22:26:03 +0300

    Update corpus_to_tokenized.py

--- corpus_to_tokenized.py
+++ corpus_to_tokenized.py
@@ -3,10 +3,9 @@
 # truncation=True
 from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
-# define go_x, x_eos
 import torch
 
-def batchify(path, N: int):
+def batchify(path, vocab, N: int):
   train_corpus = open(path, encoding='utf-8').read.splitlines()
   n = 0
   batches = []
@@ -14,8 +13,8 @@ def batchify(path, N: int):
     batch = []
     for s in train_corpus[n*N: n*N+N]:
       s = tokenizer.tokenize(s, max_length=max_seq_length, truncation=True)
-      src = [go_x] + s
-      tgt = s + [x_eos]
+      src = [vocab.go] + s
+      tgt = s + [vocab.eos]
       # word2idx, pad
       batches.append((torch.LongTensor(src).t().contiguous().to(device), \
                       torch.LongTensor(tgt).t().contiguous().to(device)))
