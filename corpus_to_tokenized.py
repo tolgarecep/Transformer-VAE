0515f2e6946fd72b6903a9783b88e61c09730045
tolgarecep <tolgarecep@protonmail.com>
2022-12-11 15:25:40 +0300

    Update corpus_to_tokenized.py

--- corpus_to_tokenized.py
+++ corpus_to_tokenized.py
@@ -2,6 +2,47 @@ import torch
 from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
 
+class Vocab(object):
+    def __init__(self, path):
+        self.word2idx = {}
+        self.idx2word = []
+
+        with open(path) as f:
+            for line in f:
+                w = line.split()[0]
+                self.word2idx[w] = len(self.word2idx)
+                self.idx2word.append(w)
+        self.size = len(self.word2idx)
+
+        self.pad = self.word2idx['<pad>']
+        self.go = self.word2idx['<go>']
+        self.eos = self.word2idx['<eos>']
+        self.unk = self.word2idx['<unk>']
+        self.blank = self.word2idx['<blank>']
+        self.nspecial = 5
+
+    @staticmethod
+    def build(sents, path, size):
+        v = ['<pad>', '<go>', '<eos>', '<unk>', '<blank>']
+        words = [w for s in sents for w in s]
+        cnt = Counter(words)
+        n_unk = len(words)
+        for w, c in cnt.most_common(size):
+            v.append(w)
+            n_unk -= c
+        cnt['<unk>'] = n_unk
+
+        with open(path, 'w') as f:
+            for w in v:
+                f.write('{}\t{}\n'.format(w, cnt[w]))
+
+def load_sent(path):
+    sents = []
+    with open(path) as f:
+        for line in f:
+            sents.append(line.split())
+    return sents
+
 def get_batch(x, vocab, device):
     go_x, x_eos = [], []
     max_len = max([len(s) for s in x])
@@ -13,12 +54,12 @@ def get_batch(x, vocab, device):
     return torch.LongTensor(go_x).t().contiguous().to(device), \
            torch.LongTensor(x_eos).t().contiguous().to(device)  # time * batch
 
-def get_tokenized_batches(data, vocab, batch_size, device):
+def get_tokenized_batches(data, max_len, vocab, batch_size, device):
     # Tokenize sentences with BERTurk tokenizer
     data_tokenized = []
     for i, s in enumerate(data):
         sentence = ' '.join(s)
-        tokenized = tokenizer.tokenize(sentence)
+        tokenized = tokenizer.tokenize(sentence, max_length=max_len, truncation=True)
         data_tokenized.append(tokenized)
 
     order = range(len(data_tokenized))
