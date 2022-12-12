5dbc135cf9b76ecc4c7308e5d8217cbf3f63dc6c
tolgarecep <tolgarecep@protonmail.com>
2022-12-12 10:10:18 +0300

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ parser.add_argument('--load-model', default='', metavar='FILE',
 # VAE arguments
 parser.add_argument('--vocab-size', type=int, default=10000, metavar='N',
                     help='keep N most frequent words in vocabulary')
-parser.add_argument('--model_type', metavar='M',
+parser.add_argument('--model_type', default='dae', metavar='M',
                     choices=['lstm', 'transformer'],
                     help='which model to learn')
 parser.add_argument('--dim_emb', type=int, default=512, metavar='D',
@@ -34,13 +34,13 @@ parser.add_argument('--dim_h', type=int, default=1024, metavar='D',
                         Transformer: dimension of encoder/decoder inputs')
 parser.add_argument('--nlayers', type=int, default=1, metavar='N',
                     help='LSTM: number of layers, \
-                        Transformer: number of encoder/decoder layers in encoder/decoder stacks')
+                        Transformer: number of encoder/decoder stacks')
 parser.add_argument('--dropout', type=float, default=0.5, metavar='DROP',
                     help='dropout probability (0 = no dropout)')
 parser.add_argument('--lambda_kl', type=float, default=0, metavar='R',
                     help='weight for kl term in VAE')
 # Transformer arguments
-parser.add_argument('--nhead', type=int, default=8, metavar='N',
+parser.add_argument('--nhead', type=int, default=6, metavar='N',
                     help='number of heads in the multiheadattention models')
 parser.add_argument('--dim_feedforward', type=int, default=2048, metavar='D',
                     help='dimension of the feedforward network model')
@@ -60,6 +60,18 @@ parser.add_argument('--no-cuda', action='store_true',
                     help='disable CUDA')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='report interval')
+
+def evaluate(model, batches):
+    model.eval()
+    meters = collections.defaultdict(lambda: AverageMeter())
+    with torch.no_grad():
+        for inputs, targets in batches:
+            losses = model.autoenc(inputs, targets)
+            for k, v in losses.items():
+                meters[k].update(v.item(), inputs.size(1))
+    loss = model.loss({k: meter.avg for k, meter in meters.items()})
+    meters['loss'].update(loss)
+    return meters
 
 def main(args):
     if not os.path.exists(args.save_dir):
@@ -108,10 +120,32 @@ def main(args):
         for i, idx in enumerate(indices):
             inputs, targets = train_batches[idx]
             losses = model.autoenc(inputs, targets, is_train=True)
+            losses['loss'] = model.loss(losses)
+            model.step(losses)
+            for k, v in losses.items():
+                meters[k].update(v.item())
 
-"""" MODEL.AUTOENC,
-LINE 194 IN VAES.PY, THEN LINE 195,196"""
+            if (i + 1) % args.log_interval == 0:
+                log_output = '| epoch {:3d} | {:5d}/{:5d} batches |'.format(
+                    epoch + 1, i + 1, len(indices))
+                for k, meter in meters.items():
+                    log_output += ' {} {:.2f},'.format(k, meter.avg)
+                    meter.clear()
+                logging(log_output, log_file)
 
+        valid_meters = evaluate(model, valid_batches)
+        logging('-' * 80, log_file)
+        log_output = '| end of epoch {:3d} | time {:5.0f}s | valid'.format(
+            epoch + 1, time.time() - start_time)
+        for k, meter in valid_meters.items():
+            log_output += ' {} {:.2f},'.format(k, meter.avg)
+        if not best_val_loss or valid_meters['loss'].avg < best_val_loss:
+            log_output += ' | saving model'
+            ckpt = {'args': args, 'model': model.state_dict()}
+            torch.save(ckpt, os.path.join(args.save_dir, 'model.pt'))
+            best_val_loss = valid_meters['loss'].avg
+        logging(log_output, log_file)
+    logging('Done training', log_file)
 
 if __name__ == '__main__':
     args = parser.parse_args()
