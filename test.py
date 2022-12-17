72c8487e1438dbe1f7059d55ac2de73b5264a4d2
tolgarecep <tolgarecep@protonmail.com>
2022-12-17 18:26:52 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+import torch
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+from vae import *
+from utils import *
+from corpus_to_tokenized import get_tokenized_batches, Vocab
+from train import evaluate
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--checkpoint', metavar='DIR', required=True,
+                    help='checkpoint directory')
+parser.add_argument('--output', metavar='FILE',
+                    help='output file name (in checkpoint directory)')
+parser.add_argument('--data', metavar='FILE',
+                    help='path to data file')
+
+parser.add_argument('--enc', default='mu', metavar='M',
+                    choices=['mu', 'z'],
+                    help='encode to mean of q(z|x) or sample z from q(z|x)')
+parser.add_argument('--dec', default='greedy', metavar='M',
+                    choices=['greedy', 'sample'],
+                    help='decoding algorithm')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                    help='batch size')
+parser.add_argument('--max-len', type=int, default=35, metavar='N',
+                    help='max sequence length for generation')
+
+def encode(sents):
+    assert args.enc == 'mu' or args.enc == 'z'
+    batches, order = get_tokenized_batches(sents, vocab, args.batch_size, device)
+    z = []
+    for enc_inputs, _, _ in batches:
+        mu, logvar = model.encode(enc_inputs)
+        if args.enc == 'mu':
+            zi = mu
+        else:
+            zi = reparameterize(mu, logvar)
+        z.append(zi.detach().cpu().numpy())
+    z = np.concatenate(z, axis=0)
+    z_ = np.zeros_like(z)
+    z_[np.array(order)] = z
+    return z_
+
+def decode(z):
+    sents = []
+    i = 0
+    while i < len(z):
+        zi = torch.tensor(z[i: i+args.batch_size], device=device)
+        outputs = model.generate(zi, args.max_len, args.dec).t()
+        for s in outputs:
+            sents.append([vocab.idx2word[id] for id in s[1:]])  # skip <go>
+        i += args.batch_size
+    return strip_eos(sents)
+
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    vocab = Vocab(os.path.join(args.checkpoint, 'vocab.txt'))
+    set_seed(args.seed)
+    cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if cuda else "cpu")
+    model = get_model(os.path.join(args.checkpoint, 'model.pt'))
+
+    if args.evaluate:
+        sents = load_sent(args.data)
+        batches, _ = get_tokenized_batches(sents, vocab, args.batch_size, device)
+        meters = evaluate(model, batches)
+        print(' '.join(['{} {:.2f},'.format(k, meter.avg)
+            for k, meter in meters.items()]))
+
+    if args.reconstruct:
+        sents = load_sent(args.data)
+        z = encode(sents)
+        sents_rec = decode(z)
+        write_z(z, os.path.join(args.checkpoint, args.output+'.z'))
+        write_sent(sents_rec, os.path.join(args.checkpoint, args.output+'.rec'))
